171f690f8df3ba24b2c53830193a120afde44d8e
Chirag Goel <61325696+chirag-goel360@users.noreply.github.com>
2021-08-22 19:09:51 +0530

    third commit

--- FormWebApp/Webforms/Registration.aspx.cs
+++ FormWebApp/Webforms/Registration.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FormWebApp.Webforms
 {
@@ -11,12 +14,48 @@ namespace FormWebApp.Webforms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
 
+                connection.Open();
+                string query = "select count(*) from UserDB where UserName='"+TextBoxName.Text+"'";
+
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                int temp = Convert.ToInt32(sqlCommand.ExecuteScalar().ToString());
+                if (temp == 1)
+                {
+                    Response.Write("User already Exist");
+                }
+                connection.Close();
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Response.Write("Registration is Successful");
+            try
+            {
+                SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+
+                connection.Open();
+                string query = "insert into UserDB (UserName,Email,Password,Country) values (@UserName, @Email, @Password, @Country)";
+
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                sqlCommand.Parameters.AddWithValue("@UserName", TextBoxName.Text);
+                sqlCommand.Parameters.AddWithValue("@Email", TextBoxEmail.Text);
+                sqlCommand.Parameters.AddWithValue("@Password", TextBoxPass.Text);
+                sqlCommand.Parameters.AddWithValue("@Country", DropDownListCountry.SelectedItem.ToString());
+
+                sqlCommand.ExecuteNonQuery();
+                Response.Redirect("Manager.aspx");
+                Response.Write("Registration Successful");
+                connection.Close();
+                //Response.Write("Registration is Successful");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error:" + ex.ToString());
+            }
         }
     }
 }
